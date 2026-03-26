2318b3b059b88cf44306b4b939fc9646649b5ed2
Sam Macbeth <sammacbeth@users.noreply.github.com>
2026-03-26 10:35:52 +0100

    Publish apple extension zip with empty scriptlets (#25)
    * Publish apple extension zip with empty scriptlets
    
    * Add browser_specific_settings as per https://app.asana.com/1/137249556945/project/481882893211075/task/1213433273303312?focus=true

--- scripts/package.mjs
+++ scripts/package.mjs
@@ -6,12 +6,22 @@ cd('src');
 await $`zip -r ../dist/content-blocker-extension-${version}.zip .`;
 echo(`Packaged content-blocker-extension-${version}.zip`);
 
+cd('..');
+await $`rm -rf dist/apple-staging`;
+await $`cp -Rp src dist/apple-staging`;
+cd('dist/apple-staging');
+await $`find scriptlets -type f -name '*.js' -exec sh -c ': > "$1"' _ {} \\;`;
+await $`zip -r ../content-blocker-extension-apple-${version}.zip .`;
+cd('..');
+await $`rm -rf apple-staging`;
+cd('..');
+echo(`Packaged content-blocker-extension-apple-${version}.zip`);
+
 const pathToPrivateKey = process.env.CRX_SIGNING_KEY;
 if (!pathToPrivateKey) {
     console.warn('CRX_SIGNING_KEY is not set. Skipping crx3 signing.');
     process.exit(0);
 }
 
-cd('../');
 await $`npx crx3-new ${pathToPrivateKey} < ./dist/content-blocker-extension-${version}.zip > ./dist/content-blocker-extension-${version}.crx`;
 echo(`Packaged content-blocker-extension-${version}.crx`);
